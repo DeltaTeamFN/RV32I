ceebb22cadadd88642989543d544050595c0f9a1
DeltaTeamFN <x90339548@gmail.com>
2026-07-25 11:06:59 +0300

    Refactor disasm.h with new header guard and comments
    Updated header guard and added function documentation.

--- disassemble/disasm.h
+++ disassemble/disasm.h
@@ -1,9 +1,25 @@
-#ifndef DISASM_H
-#define DISASM_H
+#ifndef RV32I_DISASM_H
+#define RV32I_DISASM_H
 
+#include <stddef.h>
 #include "../shared/instruction.h"
 
-// Converts the decoded instruction into a string buffer
-void rv_disasm(const rv_inst_t *inst, char *buffer, size_t size);
+#ifdef __cplusplus
+extern "C" {
+#endif
 
-#endif // DISASM_H
+/**
+ * @brief Converts a decoded RV32I instruction into human-readable assembly text.
+ *
+ * @param[in]  inst   Pointer to the decoded instruction structure.
+ * @param[out] buffer Output string buffer.
+ * @param[in]  size   Maximum allowed size of the buffer.
+ * @return int Number of characters written, or a negative value on error.
+ */
+int rv_disasm(const rv_inst_t *inst, char *buffer, size_t size);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // RV32I_DISASM_H
