74fb0bbb8a38ff713fe238793b5f949ae5a4e408
DeltaTeamFN <x90339548@gmail.com>
2026-07-25 11:22:33 +0300

    Implement elf_load function for RISC-V ELF parsing
    Added a function to parse and load RISC-V ELF files.

--- engine/elf_loader.h
+++ engine/elf_loader.h
@@ -1,8 +1,25 @@
 #ifndef ENGINE_ELF_LOADER_H
 #define ENGINE_ELF_LOADER_H
 
+#include <stdbool.h>
 #include "memory.h"
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * @brief Parses a 32-bit RISC-V ELF file and loads executable segments into memory.
+ *
+ * @param[in]  filepath    Path to the .elf binary file.
+ * @param[out] mem         Pointer to target memory structure.
+ * @param[out] entry_point Pointer where the execution start address (PC) will be stored.
+ * @return true on successful parsing and memory load, false on error.
+ */
 bool elf_load(const char *filepath, memory_t *mem, rv_addr_t *entry_point);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif // ENGINE_ELF_LOADER_H
